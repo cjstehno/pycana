16b44074856c5418aaf2d9c9c76b9eebcc376fa6
Chris Stehno <chris.stehno@netscout.com>
2023-06-30 11:08:55 -0500

    reworking

--- pycana/console_formatter.py
+++ pycana/console_formatter.py
@@ -1,3 +1,6 @@
+"""
+Configuration for the rich console.
+"""
 from rich.console import Console
 
 
@@ -5,8 +8,10 @@ console = Console()
 
 
 def format_success(message):
+    """Formats the success messages."""
     return console.print(f"[bold green]{message}[/bold green]")
 
 
 def format_error(message):
+    """Formats the error messages."""
     return console.print(f"[bold red]{message}[/bold red]")
